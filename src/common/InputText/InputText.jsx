f6c09ed43dde756e11d606004e79d52aa95095ad
Ruben <rgolfe81@gmail.com>
2023-03-14 19:52:59 +0100

    feat: Include inpunts in Login

--- src/common/InputText/InputText.jsx
+++ src/common/InputText/InputText.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./InputText.css";
 
-export const InputText = () => {
-    return 
-    <div className="inputTextDesign">
-    </div>;
+export const InputText = ({
+    type,
+    placeholder,
+    name,
+    changeFunction,
+    validateFunction
+}) => {
+    return (
+    <>
+    <input className="inputTextDesign"
+    type={type}
+    placeholder={placeholder}
+    name={name}
+    changeFunction={changeFunction}
+    validateFunction={validateFunction}
+    />
+    </>
+    )
 };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,10 +5,20 @@ import "./Login.css";
 export const Login = () => {
     return (
         <div className="loginDesign">
+            <div className='titleDesign'>
+                <h2>Login</h2>   
+            </div>
             <InputText
             type="email"
             name="email"
             placeholder="escribe un email"
+            // changeFunction={(e) => inputHandler(e)}
+            // validateFunction={(e) => inputValidate(e)}
+            />
+            <InputText
+            type="password"
+            name="password"
+            placeholder="escribe el password"
             // changeFunction={(e) => inputHandler(e)}
             // validateFunction={(e) => inputValidate(e)}
             />
